4e554566c31d03da5533feb202893773fd65ed1d
pnhung177 <pnhung177@acegik.net>
2017-08-05 09:14:58 +0700

    Update version opflow-core 0.1.4

--- src/main/java/com/devebot/opflow/example/FibonacciRpcMaster.java
+++ src/main/java/com/devebot/opflow/example/FibonacciRpcMaster.java
@@ -4,6 +4,7 @@ import com.devebot.opflow.OpflowHelper;
 import com.devebot.opflow.OpflowMessage;
 import com.devebot.opflow.OpflowRpcMaster;
 import com.devebot.opflow.OpflowRpcRequest;
+import com.devebot.opflow.OpflowRpcResult;
 import com.devebot.opflow.OpflowUtil;
 import com.devebot.opflow.exception.OpflowConstructorException;
 import java.util.Map;
@@ -39,18 +40,24 @@ public class FibonacciRpcMaster {
         
         System.out.println("[+] ExampleMaster request");
 
-        OpflowRpcRequest result1 = rpc.request(20);
-        OpflowRpcRequest result2 = rpc.request(30);
+        OpflowRpcRequest req1 = rpc.request(20);
+        OpflowRpcRequest req2 = rpc.request(30);
         
-        while(result1.hasNext()) {
-            OpflowMessage msg = result1.next();
-            System.out.println("[-] message1 received: " + msg.getContentAsString());
+        // OpflowRpcRequest object works as an Interator<OpflowMessage>
+        while(req1.hasNext()) {
+            OpflowMessage msg = req1.next();
+            System.out.println("[-] message1 received: " + msg.getContentAsString() + 
+                    " / workerTag: " + msg.getInfo().get("workerTag"));
         }
+        System.out.println();
         
-        while(result2.hasNext()) {
-            OpflowMessage msg = result2.next();
-            System.out.println("[-] message2 received: " + msg.getContentAsString());
+        // Transforms OpflowRpcRequest object to OpflowRpcResult object
+        OpflowRpcResult result2 = OpflowUtil.exhaustRequest(req2);
+        System.out.println("[-] message2 worker: " + result2.getWorkerTag());
+        for(OpflowRpcResult.Step step: result2.getProgress()) {
+            System.out.println("[-] message2 percent: " + step.getPercent());
         }
+        System.out.println("[-] message2 result: " + result2.getValueAsString());
         
         System.out.println("[-] ExampleMaster has finished");
     }
