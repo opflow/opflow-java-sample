bbc018cbff18eb48a3dcd118954a95f9534f248d
pnhung177 <pnhung177@gmail.com>
2020-08-06 11:57:11 +0700

    Add the serverlet config validation to the worker example

--- worker/src/main/java/com/devebot/opflow/sample/FibonacciWorker.java
+++ worker/src/main/java/com/devebot/opflow/sample/FibonacciWorker.java
@@ -2,13 +2,11 @@ package com.devebot.opflow.sample;
 
 import com.devebot.opflow.OpflowBuilder;
 import com.devebot.opflow.OpflowConfig;
+import com.devebot.opflow.OpflowConfigValidator;
 import com.devebot.opflow.OpflowServerlet;
-import com.devebot.opflow.exception.OpflowConfigValidationException;
 import com.devebot.opflow.exception.OpflowConnectionException;
 import com.devebot.opflow.sample.services.AlertSenderImpl;
 import com.devebot.opflow.sample.services.FibonacciCalculatorImpl;
-import com.devebot.opflow.supports.OpflowJsonTool;
-import java.util.Map;
 
 /**
  *
@@ -16,15 +14,11 @@ import java.util.Map;
  */
 public class FibonacciWorker {
     public static void main(String[] argv) throws Exception {
+        OpflowConfig.Validator validator = OpflowConfigValidator
+            .getServerletConfigValidator(FibonacciWorker.class.getResourceAsStream("/worker-schema.json"));
         try {
             System.out.println("[+] FibonacciWorker start:");
-            final OpflowServerlet serverlet = OpflowBuilder.createServerlet("worker.properties", new OpflowConfig.Validator() {
-                @Override
-                public Object validate(Map<String, Object> configuration) throws OpflowConfigValidationException {
-                    System.out.println(OpflowJsonTool.toString(configuration, true));
-                    return null;
-                }
-            });
+            final OpflowServerlet serverlet = OpflowBuilder.createServerlet("worker.properties", validator);
             serverlet.instantiateType(AlertSenderImpl.class);
             serverlet.instantiateType(FibonacciCalculatorImpl.class);
             serverlet.serve();
